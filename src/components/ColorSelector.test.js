ab3918719e01493e64a6efd590df93ecd12b815c
Leslie Shea <leslieyshea@gmail.com>
2019-05-22 10:26:55 -0700

    fixed tests

--- src/components/ColorSelector.test.js
+++ src/components/ColorSelector.test.js
@@ -4,7 +4,7 @@ import ColorSelector from './ColorSelector';
 
 describe('ColorSelector component', () => {
   it('renders a ColorSelector', () => {
-    const wrapper = shallow(<ColorSelector />);
+    const wrapper = shallow(<ColorSelector color="red" backgroundColor="red" handleChange={() => <h1>hello</h1> } />);
     expect(wrapper).toMatchSnapshot();
   });
 });
--- src/components/TextSelector.test.js
+++ src/components/TextSelector.test.js
@@ -4,7 +4,7 @@ import TextSelector from './TextSelector';
 
 describe('TextSelector component', () => {
   it('renders a TextSelector', () => {
-    const wrapper = shallow(<TextSelector />);
+    const wrapper = shallow(<TextSelector text="hi" handleChange={() => <h1>hello</h1>} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
